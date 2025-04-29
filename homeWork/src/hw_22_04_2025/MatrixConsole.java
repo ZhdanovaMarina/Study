e121ed95b209921b70f5b9fec2e4e2fe7e55f693
ZhdanovaMarina <wernelu1@mail.ru>
2025-04-29 12:21:52 +0400

    Внесены правки в домашнее задание

--- homeWork/src/hw_22_04_2025/MatrixConsole.java
+++ homeWork/src/hw_22_04_2025/MatrixConsole.java
@@ -7,58 +7,93 @@ import java.util.Scanner;
 public class MatrixConsole {
     public static void main(String[] args) {
 
-        int[][] array1 = new int[3][3];
-        int[][] array2 = new int[3][3];
-
-        Random random = new Random();
-
+        int[][] matrix = new int[3][3];
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число:\n 1. Заполнить случайно\n 2. Вывести\n " +
-                "3. Найти все простые элементы\n 4. Сложить с другой матрицей\n " +
-                "0. Выход");
-        String number = scanner.nextLine();
 
-        switch (number) {
-            case "1":
-                for (int i = 0; i < array1.length; i++) {
-                    for (int j = 0; j < array1.length; j++) {
-                        array1[i][j] = random.nextInt(10);
-                    }
-                }
-                break;
-            case "2":
-                for (int i = 0; i < array1.length; i++) {
-                    for (int j = 0; j < array1.length; j++) {
+        while (true){
 
-                    }
-                    System.out.println(Arrays.deepToString(array1));
-                }
-                break;
-            case "3":
-                int num = 0;
-                for (int i = 0; i < array1.length; i++) {
-                    for (int j = 0; j < array1.length; j++) {
-                        if (array1[i][j] % ){
-                            num = array1[i][j];
-                            System.out.println(num);
+            printMenu();
+            String number = scanner.next();
+
+            switch (number) {
+                case "1":
+
+                    System.out.println("Введите размер матрицы (x, y): ");
+                    int x = scanner.nextInt();
+                    int y = scanner.nextInt();
+                    matrix = new int[x][y];
+
+                    Random random = new Random();
+                    for (int i = 0; i < matrix.length; i++) {
+                        for (int j = 0; j < matrix.length; j++) {
+                            matrix[i][j] = random.nextInt(10);
                         }
                     }
-                }
-                break;
-            case "4":
+                    break;
+                case "2":
+                    for(int[] row : matrix){
+                        System.out.println(Arrays.toString(row));
+                    }
+                    break;
+                case "3":
+                    System.out.println("Простые числа: ");
+                    for (int i = 0; i < matrix.length; i++) {
+                        for (int j = 0; j < matrix.length; j++) {
+                            //91
+                            boolean isPrime = isPrime(matrix[i][j]);
+                            if (isPrime){
+                                System.out.print(matrix[i][j] +" ");
+                            }
+                        }
+                    }
+                    System.out.println();
+                    break;
+                case "4":
+                    int rows = matrix.length;
+                    int cols = matrix[0].length;
 
-                break;
-            case "5":
-                System.out.println("До встречи!");
-                break;
-            default:
-                System.out.println("Вы ввели неверное число!");
+                    System.out.println("Введите " + rows*cols + " чисел: " );
 
+                    for (int i = 0; i < matrix.length; i++) {
+                        for (int j = 0; j < matrix.length; j++) {
+                            matrix[i][j] += scanner.nextInt();
+                        }
+                    }
+                    break;
+                case "0":
+                    System.out.println("До встречи!");
+                    return;
+                default:
+                    System.out.println("Вы ввели неверное число!");
+
+            }
         }
 
     }
 
+    private static boolean isPrime(int number) {
+        if(number<=2){
+            return false;
+        }
+        boolean isPrime = true;
+        for (int k = 2; k < number; k++) {
+            if (number % k == 0){
+                isPrime = false;
+            }
+        }
+        return isPrime;
+    }
 
+    private static void printMenu() {
+        System.out.println(
+                        "Введите число:\n " +
+                        "1. Заполнить случайно\n " +
+                        "2. Вывести\n " +
+                        "3. Найти все простые элементы\n " +
+                        "4. Сложить с другой матрицей\n " +
+                        "0. Выход"
+        );
+    }
 
 }
