7b3b88cbe1957b64884c6c59e46c7c1d088e2af5
ZhdanovaMarina <wernelu1@mail.ru>
2025-05-21 11:43:53 +0400

    внесены правки в домашнее задание

--- homeWork/src/hw_18_05_2025/task4/Main.java
+++ homeWork/src/hw_18_05_2025/task4/Main.java
@@ -3,29 +3,30 @@ package hw_18_05_2025.task4;
 public class Main {
     public static void main(String[] args) {
 
-
+    int number = 3;
+    grade(number);
 
     }
 
-//    public static int grade(int score){
+    public static void grade(int score){
 
-//        switch (score){
-//            case 0:
-//                System.out.println("Ваша оценка: A (отлично)");
-//                break;
-//            case 1:
-//                System.out.println("Ваша оценка: B (хорошо)");
-//                break;
-//            case 2:
-//                System.out.println("Ваша оценка: C (удовлетворительно)");
-//                break;
-//            case 3:
-//                System.out.println("Ваша оценка: D (плохо)");
-//                break;
-//            default:
-//                System.out.println("Ваша оценка: F (неудовлетворительно)");
-//        }
-//        return
-//    }
-
+        switch (score){
+            case 0:
+                System.out.println("Ваша оценка: A (отлично)");
+                break;
+            case 1:
+                System.out.println("Ваша оценка: B (хорошо)");
+                break;
+            case 2:
+                System.out.println("Ваша оценка: C (удовлетворительно)");
+                break;
+            case 3:
+                System.out.println("Ваша оценка: D (плохо)");
+                break;
+            case 4:
+                System.out.println("Ваша оценка: F (неудовлетворительно)");
+            default:
+                System.out.println("Введена неверная цифра!");
+        }
+    }
 }
